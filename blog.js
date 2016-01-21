c3bb32e82c7ed7a0d2d5a2bef42d4f4455fc4110
Azureaus <liam.oshea@o2-v2.com>
2016-01-21 17:19:11 +0000

    remove grid and colum class from homepage blog section

--- blog.js
+++ blog.js
@@ -13,14 +13,18 @@ function blogobject(title,date,content){
 function blogcontent(){
   title = ['blog1','blog2','blog3','blog4','blog5'];
   date = ['21/01/16','22/02/16','23/02/16','24/02/16','25/02/16'];
-  content = ['blog1 content','blog2 content','blog3 content','blog4 content','blog5 content'];
+  content = ['Bacon ipsum dolor amet tri-tip ribeye rump salami, jowl shank short ribs meatball chicken. Frankfurter kevin kielbasa, capicola spare ribs corned beef pork belly sausage cupim short loin pork ham hock chicken pork loin.',
+'Bacon ipsum dolor amet tri-tip ribeye rump salami, jowl shank short ribs meatball chicken. Frankfurter kevin kielbasa, capicola spare ribs corned beef pork belly sausage cupim short loin pork ham hock chicken pork loin.',
+'Bacon ipsum dolor amet tri-tip ribeye rump salami, jowl shank short ribs meatball chicken. Frankfurter kevin kielbasa, capicola spare ribs corned beef pork belly sausage cupim short loin pork ham hock chicken pork loin.',
+'Bacon ipsum dolor amet tri-tip ribeye rump salami, jowl shank short ribs meatball chicken. Frankfurter kevin kielbasa, capicola spare ribs corned beef pork belly sausage cupim short loin pork ham hock chicken pork loin.',
+'Bacon ipsum dolor amet tri-tip ribeye rump salami, jowl shank short ribs meatball chicken. Frankfurter kevin kielbasa, capicola spare ribs corned beef pork belly sausage cupim short loin pork ham hock chicken pork loin.'];
   blogs = [];
   for (var i=0; i<title.length; i++){
     blogs[i] = new blogobject(title[i],date[i],content[i]);
   }
   for (i=0; i<blogs.length; i++){
     document.write('<article>');
-    document.write('<h3>'+blogs[i].title+'</h3>');
+    document.write('<h2>'+blogs[i].title+'</h2>');
     document.write('<p>'+blogs[i].content+'</p>');
     document.write('<p>'+blogs[i].date+'</p>');
     document.write('</article>');
